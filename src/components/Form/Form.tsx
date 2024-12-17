0f8013b8db152435a4ac3d0d88dd873544d40ae6
Johnny <jonathanbc2012@gmail.com>
2024-12-17 12:22:47 +0100

    feat: form styles modified

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,26 +1,27 @@
 import { countries } from "../../data/countries";
+import styles from "./Form.module.css";
 
 export default function Form() {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
         <input id="city" type="text" name="city" placeholder="ciudad" />
       </div>
 
-      <div>
+      <div className={styles.field}>
         <label htmlFor="city">Pais:</label>
         <select>
-        <option value="">--- Selecciona un País ---</option>
-        {countries.map((country) => (
-          <option key={country.code} value={country.name}>
-            {country.name}
-          </option>
-        ))}
+          <option value="">--- Selecciona un País ---</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.name}>
+              {country.name}
+            </option>
+          ))}
         </select>
       </div>
 
-      <input type="submit" value="consultar tiempo" />
+      <input className={styles.submit} type="submit" value="consultar tiempo" />
     </form>
   );
 }
